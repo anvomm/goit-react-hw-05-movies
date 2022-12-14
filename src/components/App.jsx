97b7c221a8da5bfa85b6bbf1040e4304fd16809c
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-12-15 00:47:05 +0200

    unused imports removed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
 import { HomePageView } from 'pages/HomePageView';
-import {
-  getTrendingMovies,
-  getMovieByQuery,
-  getOneMovieById,
-  getMovieActors,
-  getMovieReviews,
-} from 'services/moviesAPI';
 
 export const App = () => {
   return (
