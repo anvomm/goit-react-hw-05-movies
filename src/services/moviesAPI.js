fb16c8c7bfacadd7c2755ea9c4e80c2728e5563e
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-12-14 16:20:59 +0200

    axios installed

---
+++ src/services/moviesAPI.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/';
+const API_KEY = 'd929b7a4b435aa22496bb0793b172bfc';
+
+export async function getTrendingMovies() {
+  try {
+    const { data } = await axios.get('3/trending/movie/day', {
+      params: {
+        api_key: API_KEY,
+      },
+    });
+    return data.results;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getMovieByQuery(query, page = 1) {
+  try {
+    const { data } = await axios.get('3/search/movie', {
+      params: {
+        api_key: API_KEY,
+        query,
+        page,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getOneMovieById(id) {
+  try {
+    const { data } = await axios.get(`3/movie/${id}`, {
+      params: {
+        api_key: API_KEY,
+      },
+    });
+    return data.results;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getMovieActors(id) {
+  try {
+    const { data } = await axios.get(`3/movie/${id}/credits`, {
+      params: {
+        api_key: API_KEY,
+      },
+    });
+    return data.results;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getMovieReviews(id) {
+  try {
+    const { data } = await axios.get(`3/movie/${id}/reviews`, {
+      params: {
+        api_key: API_KEY,
+      },
+    });
+    console.log(data);
+    return data.results;
+  } catch (error) {
+    console.error(error);
+  }
+}
