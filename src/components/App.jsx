6cef6482b58ee2501c85c17e7701f363054b2134
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-12-14 16:21:28 +0200

    moviesAPI done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,17 @@
+import {
+  getTrendingMovies,
+  getMovieByQuery,
+  getOneMovieById,
+  getMovieActors,
+  getMovieReviews,
+} from 'services/moviesAPI';
+
+/* getMovieByQuery('cat');
+
+getOneMovieById(991833); 
+getMovieActors(991833);*/
+getMovieReviews(991833);
+
 export const App = () => {
   return (
     <div
@@ -7,7 +21,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
