3ccdebe6888b03f2290107fe9eb4d0feb0b97ac0
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-12-15 01:11:11 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
 import { HomePageView } from 'pages/HomePageView';
 
 export const App = () => {
   return (
-    <div>
-      {' '}
-      HELLO
+    <>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePageView />} />
         </Route>
       </Routes>
-    </div>
+    </>
   );
 };
