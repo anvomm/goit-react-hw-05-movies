5287da65b7ec9771a444e32fde43641892efaae5
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-12-17 00:27:59 +0200

    default image addrd

--- src/components/ActorCard/ActorCard.jsx
+++ src/components/ActorCard/ActorCard.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
+
 export const ActorCard = ({ name, picture, character }) => {
+  const getImgUrl = image => {
+    if (image === null) {
+      return 'https://img.icons8.com/ios-filled/512/gender-neutral-user.png';
+    }
+    return `https://image.tmdb.org/t/p/w200${image}`;
+  };
   return (
     <>
-      <img src={`https://image.tmdb.org/t/p/w200${picture}`} alt={name} />
+      <img src={getImgUrl(picture)} alt={name} width="200" />
       <h3>{name}</h3>
       <p>{character}</p>
     </>
