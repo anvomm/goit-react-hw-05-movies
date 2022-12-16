3132fbd34dfc6583ab3d933a6f8584d27e031413
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-12-16 15:56:12 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
 import { HomePageView } from 'pages/HomePageView';
 import { MoviesPageView } from 'pages/MoviesPageView';
+import { MovieDetailsPageView } from 'pages/MovieDetailsPageView';
+import { CastSubPage } from 'pages/CastSubPage';
+import { ReviewsSubPage } from 'pages/ReviewsSubPage';
 import { NotFound } from 'pages/NotFound';
 
 export const App = () => {
@@ -10,6 +13,10 @@ export const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePageView />} />
         <Route path="movies" element={<MoviesPageView />} />
+        <Route path="movies/:movieId" element={<MovieDetailsPageView />}>
+          <Route path="cast" element={<CastSubPage />} />
+          <Route path="reviews" element={<ReviewsSubPage />} />
+        </Route>
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
