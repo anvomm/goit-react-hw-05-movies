3f63af14bf1671c798269c76df3026b17d01011c
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-12-19 01:19:17 +0200

    go back button done

---
+++ src/components/GoBackButton/GoBackButton.jsx
@@ -0,0 +1,23 @@
+import { useNavigate, useLocation } from 'react-router-dom';
+import { BackButton } from './GoBackButton.styled';
+import { IconContext } from 'react-icons';
+import { HiOutlineArrowNarrowLeft } from 'react-icons/hi';
+
+export const GoBackButton = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  return (
+    <BackButton
+      type="button"
+      onClick={() => {
+        navigate(location?.state?.from ?? '/');
+      }}
+    >
+      <IconContext.Provider value={{ size: 30, color: '#5CE4CF' }}>
+        <HiOutlineArrowNarrowLeft />
+      </IconContext.Provider>
+      Go back
+    </BackButton>
+  );
+};
---
+++ src/components/GoBackButton/GoBackButton.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const BackButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  background-color: inherit;
+  text-transform: uppercase;
+  color: #99a9ff;
+  font-size: 16px;
+`;
