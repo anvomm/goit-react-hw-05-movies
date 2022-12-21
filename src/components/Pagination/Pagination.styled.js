8ae6f581043ab9cd9181051c4ba1adc0fb6e009b
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-12-21 13:24:22 +0200

    code replaced

--- src/components/Pagination/Pagination.styled.js
+++ src/components/Pagination/Pagination.styled.js
@@ -2,12 +2,48 @@ import ReactPaginate from 'react-paginate';
 import styled from 'styled-components';
 
 export const Pagination = styled(ReactPaginate)`
-  width: 300px;
+  width: 280px;
   margin: 70px auto 0 auto;
   display: flex;
   justify-content: space-between;
   cursor: pointer;
   color: #99a9ff;
+
+  li.disabled a {
+    color: grey;
+  }
+  li.disable,
+  li.disabled a {
+    cursor: default;
+  }
+
+  li.active a {
+    color: #1d1d1d;
+  }
+
+  li.page-item {
+    width: 25px;
+    height: 25px;
+    font-size: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    @media (min-width: 768px) {
+      width: 35px;
+      height: 35px;
+    }
+
+    @media (min-width: 1280px) {
+      width: 40px;
+      height: 40px;
+    }
+  }
+
+  li.page-item.active {
+    border-radius: 50%;
+    background-color: #99a9ff;
+  }
 
   @media (min-width: 768px) {
     width: 500px;
--- src/utils/GlobalStyles.js
+++ src/utils/GlobalStyles.js
@@ -43,34 +43,4 @@ body {
     display: flex;
     flex-direction: column;
 }
-
-li.active a {
-    color: #1D1D1D;
-  }
-
-  li.page-item {
-    width: 30px;
-    height: 30px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-     @media (min-width: 768px) {
-    width: 35px;
-    height: 35px;
-  }
-
-    @media (min-width: 1280px) {
-    width: 40px;
-    height: 40px;
-  }
-
-  }
-
-  li.page-item.active {
-    
-    border-radius: 50%;
-    background-color: #99a9ff;
-    
-  }
 `;
